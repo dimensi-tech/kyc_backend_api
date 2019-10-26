ae6c3dc3dfcf8a603dc82dc289005e65f8ef0470
Persada Ersad <ersad40@gmail.com>
2019-10-26 14:22:02 +0700

    Initial Setup Capistrano
     - Update Config Capistrano
    
     - Add automation deployment
    
    Trello: https://trello.com/c/zEZlfkto

---
+++ config/deploy.rb
@@ -0,0 +1,40 @@
+# config valid for current version and patch releases of Capistrano
+lock '~> 3.11.1'
+
+set :application, 'kyc_backend_api'
+set :repo_url, 'git@github.com:dimensi-tech/kyc_backend_api.git'
+set :deploy_to, '/home/ubuntu/apps/kyc_backend_api'
+set :branch, 'master'
+set :bundle_path, nil
+set :bundle_flags, '--system --quiet'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', '.env'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,1 @@
+server '134.209.109.172', user: 'deploy', roles: %w[app db web]
