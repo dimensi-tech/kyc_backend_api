44174190c95da2f6b19c54e3718c68dfcbeddfe0
Persada Ersad <ersad40@gmail.com>
2019-10-29 20:05:35 +0700

    Fix Configuration Capistratno
     - Fixed configuration capistrano deployment
    
    Trello: https://trello.com/c/53Sbemnx

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,19 @@
 server '134.209.109.172', user: 'deploy', roles: %w[app db web]
+set :application, 'kyc_backend_api'
+set :migration_role, :db
+set :migration_servers, -> { primary(fetch(:migration_role)) }
+set :conditionally_migrate, true
+set :stage, :production
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+set :pty,             true
+set :use_sudo,        false
+set :deploy_via,      :remote_cache
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true # Change to false when not using ActiveRecord
